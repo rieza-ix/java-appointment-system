cb236d93d159bd5f2ac52177f635268764f86959
rieza-ix <126137210+rieza-ix@users.noreply.github.com>
2023-09-21 16:35:43 +0800

    :tada: feat: start of Java Appointment System Project

---
+++ build/classes/Login.rs
@@ -0,0 +1,5 @@
+Main.Login$3
+Main.Login$2
+Main.Login$1
+Main.Login$4
+Main.Login
---
+++ src/App.java
@@ -0,0 +1,8 @@
+// @author riezxcvii
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
